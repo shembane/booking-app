26666e6b2673e7631fe57e584fc05c2854e58bf2
Aaron <shem@shembane.com>
2023-10-28 13:50:57 +0300

    Continued the code a bit

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module booking-app
 
-go 1.20
+go 1.21.3
--- main.go
+++ main.go
@@ -11,4 +11,11 @@ func main() { // This function tells the compiler where to start running the app
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v has booked %v tickets\n", userName, userTickets)
+
 }
